43274d0cde6053a28c9462ca53f1467dc674c5b2
Bikathi Martin <bikathimartin@outlook.com>
2023-08-10 12:33:00 +0300

    Globally import Auto-Animate plugin and Vue Router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import { createPinia } from 'pinia';
+import router from './configs/router';
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
 
 const pinia = createPinia();
 
-createApp(App).use(pinia).mount('#app');
+createApp(App).use(pinia).use(autoAnimatePlugin).use(router).mount('#app');
