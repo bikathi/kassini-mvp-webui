0598d9c91ca54a3fb9b253ab96b5c684eb2c411b
Bikathi Martin <bikathimartin@outlook.com>
2023-08-10 12:32:11 +0300

    Create router and Home view

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<script setup>
+	import { ref } from 'vue';
+	const items = ref(['😏', '😐', '😑', '😒', '😕']);
+	function removeItem(toRemove) {
+		items.value = items.value.filter((item) => item !== toRemove);
+	}
+
+	function addItem() {
+		items.value.push('🤣');
+	}
+</script>
+
+<template>
+	<h5>Click emojis to remove them.</h5>
+	<ul v-auto-animate="{ duration: 500 }">
+		<li
+			v-for="item in items"
+			:key="item"
+			@click="removeItem(item)">
+			{{ item }}
+		</li>
+	</ul>
+	<button
+		class="dui-btn"
+		@click="addItem()">
+		Add item to list
+	</button>
+</template>
+Vue
