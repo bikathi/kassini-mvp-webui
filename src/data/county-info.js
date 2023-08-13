e536b83fa833133a985d16b030df062c942ae6b8
Bikathi Martin <bikathimartin@outlook.com>
2023-08-13 21:12:09 +0300

    Add folder for in-app data

---
+++ src/data/county-info.js
@@ -0,0 +1,49 @@
+const countiesAndConstituencies = [
+	{
+		id: 1,
+		name: 'Mombasa',
+		constituencies: [
+			'Changamwe',
+			'Jomvu',
+			'Kisauni',
+			'Nyali',
+			'Likoni',
+			'Mvita',
+		],
+	},
+	{
+		id: 2,
+		name: 'Kwale',
+		constituencies: ['Msambweni', 'Lunga Lunga', 'Matuga', 'Kinango'],
+	},
+	{
+		id: 3,
+		name: 'Kilifi',
+		constituencies: [
+			'Kilifi North',
+			'Kilifi South',
+			'Kaloleni',
+			'Rabai',
+			'Ganze',
+			'Malindi',
+			'Magarini',
+		],
+	},
+	{
+		id: 4,
+		name: 'Tana River',
+		constituencies: ['Garsen', 'Galole', 'Bura'],
+	},
+	{
+		id: 5,
+		name: 'Lamu',
+		constituencies: ['Lamu East', 'Lamu West'],
+	},
+	{
+		id: 6,
+		name: 'Taita Taveta',
+		constituencies: ['Taveta', 'Wundanyi', 'Mwatate', 'Voi'],
+	},
+];
+
+export default countiesAndConstituencies;
