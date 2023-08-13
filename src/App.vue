66404a09e6f6f691ad61ac1b095ca7193c798cdf
Bikathi Martin <bikathimartin@outlook.com>
2023-08-13 23:04:06 +0300

    Create and mount custom notification system

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
-<script setup></script>
+<script setup>
+	import { ref } from 'vue';
+
+	const openNotification = ref(false);
+	const notificationMessage = ref('');
+	function displayNotification(data) {
+		notificationMessage.value = data;
+		openNotification.value = true;
+
+		setTimeout(() => {
+			openNotification.value = false;
+		}, 5000);
+	}
+</script>
 
 <template>
-	<router-view></router-view>
+	<div
+		class="fixed bg-transparent flex justify-center items-center p-1 top-0 w-screen"
+		v-auto-animate="{ duration: 300 }">
+		<BannerNotification
+			v-if="openNotification"
+			:message="notificationMessage" />
+	</div>
+	<router-view @displayNotification="displayNotification"></router-view>
 </template>
---
+++ src/components/BannerNotification.vue
@@ -0,0 +1,15 @@
+<script setup>
+	const componentProps = defineProps({
+		message: {
+			type: String,
+			required: true,
+			default: 'Sample Message',
+		},
+	});
+</script>
+
+<template>
+	<div class="alert alert-info w-fit text-center">
+		<h1>{{ message }}</h1>
+	</div>
+</template>
