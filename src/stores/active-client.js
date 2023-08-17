61fc20302a4a74276f0749bce7570eff5c19958a
Bikathi Martin <bikathimartin@outlook.com>
2023-08-17 15:28:10 +0300

    Add logic to move authToken to session storage

--- src/stores/active-client.js
+++ src/stores/active-client.js
@@ -1,21 +1,33 @@
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
+import { useStorage } from '@vueuse/core';
 
 const useActiveClientStore = defineStore('activeClientStore', () => {
-	const clientDetails = ref({});
+	const clientDetails = useStorage('clientDetails', ref({}), localStorage);
 
 	function setClientDetails(logginDetails) {
 		clientDetails.value = logginDetails;
+	}
+
+	async function setJWTInSessionStorage() {
+		const jwtToken = useStorage('authToken', clientDetails.value.authToken, sessionStorage);
+
+		// remove it from the local storage
+		clientDetails.value.authToken = null;
 	}
 
 	function logoutClient() {
 		clientDetails.value = {};
 	}
 
+	const clientRoles = computed(() => clientDetails.value.roles);
+
 	return {
 		clientDetails,
 		setClientDetails,
 		logoutClient,
+		clientRoles,
+		setJWTInSessionStorage,
 	};
 });
 
