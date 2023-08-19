21f55e194752128d0c3a8071de1765cff331f799
Bikathi Martin <bikathimartin@outlook.com>
2023-08-19 05:43:05 +0300

    Add logic to show dashboard routes based on roles

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,10 @@
 	import { Icon } from '@iconify/vue';
 	import { useRoute } from 'vue-router';
 	import { computedAsync } from '@vueuse/core';
+	import ApplicationInit from '../components/ApplicationInit.vue';
+	import useActiveClientStore from '../stores/active-client.js';
+	import { storeToRefs } from 'pinia';
+	import { computed } from 'vue';
 
 	const inAppPages = [
 		{ text: 'Posts', icon: 'system-uicons:home-alt', to: 'Posts' },
@@ -25,54 +29,71 @@
 	const activePageName = computedAsync(async () => {
 		return route.name;
 	});
+
+	// decide exactly what parts of the UI will be shown based on client roles
+	const activeClientStore = useActiveClientStore();
+	const { clientRoles } = storeToRefs(activeClientStore);
+	const computedAppPages = computed(() => {
+		if (clientRoles.value.length === 1) {
+			inAppPages.splice(1, 1);
+			return inAppPages;
+		}
+
+		return inAppPages;
+	});
 </script>
 
 <template>
 	<main class="h-screen flex justify-center">
-		<div class="h-screen w-full lg:w-[55%] flex flex-col">
-			<div class="h-[12%]">
-				<div class="flex justify-between py-1">
-					<h1 class="text-3xl tracking-wide font-inter">
-						{{ activePageName }}
-					</h1>
-					<div class="dropdown dropdown-left">
-						<label
-							tabindex="0"
-							class="btn btn-circle">
+		<Suspense>
+			<div class="h-screen w-full lg:w-[55%] flex flex-col">
+				<div class="h-[12%]">
+					<div class="flex justify-between py-1">
+						<h1 class="text-3xl tracking-wide font-inter">
+							{{ activePageName }}
+						</h1>
+						<div class="dropdown dropdown-left">
+							<label
+								tabindex="0"
+								class="btn btn-circle">
+								<Icon
+									icon="system-uicons:menu-horizontal"
+									width="25" />
+							</label>
+							<ul
+								tabindex="0"
+								class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 border border-base-200/50">
+								<li>
+									<router-link :to="{ name: 'Login' }">Logout</router-link>
+								</li>
+								<li>
+									<router-link :to="{ name: 'Settings' }">Settings</router-link>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="flex justify-around">
+						<router-link
+							v-for="(page, index) in computedAppPages"
+							:key="index"
+							:to="{ name: page.to }"
+							class="inline-flex flex-col items-center w-full"
+							:class="route.name === page.to ? 'border-b-2' : ''">
 							<Icon
-								icon="system-uicons:menu-horizontal"
+								:icon="page.icon"
 								width="25" />
-						</label>
-						<ul
-							tabindex="0"
-							class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 border border-base-200/50">
-							<li>
-								<router-link :to="{ name: 'Login' }">Logout</router-link>
-							</li>
-							<li>
-								<router-link :to="{ name: 'Settings' }">Settings</router-link>
-							</li>
-						</ul>
+							{{ page.text }}
+						</router-link>
 					</div>
 				</div>
-				<div class="flex justify-around">
-					<router-link
-						v-for="(page, index) in inAppPages"
-						:key="index"
-						:to="{ name: page.to }"
-						class="inline-flex flex-col items-center w-full"
-						:class="route.name === page.to ? 'border-b-2' : ''">
-						<Icon
-							:icon="page.icon"
-							width="25" />
-						{{ page.text }}
-					</router-link>
+				<div class="flex-grow">
+					<!-- the dashboard views go here -->
+					<router-view></router-view>
 				</div>
 			</div>
-			<div class="flex-grow">
-				<!-- the dashboard views go here -->
-				<router-view></router-view>
-			</div>
-		</div>
+			<template #fallback>
+				<ApplicationInit />
+			</template>
+		</Suspense>
 	</main>
 </template>
